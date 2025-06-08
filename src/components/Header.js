1475e0a058810a69bf37e19f87a2eab455a3fa1d
Danny <dannyabdelbary@gmail.com>
2025-06-08 18:55:40 -0400

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 const Header = () => {
   return (
